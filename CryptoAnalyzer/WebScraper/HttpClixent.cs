3f4e59b632730eb1895d7971b0f7f4c9b77c00e0
Rytis Cepulis <cepRytis@gmail.com>
2021-05-22 21:55:46 +0300

    init

---
+++ CryptoAnalyzer/WebScraper/HttpClixent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    internal class HttpClixent
+    {
+        public HttpClixent()
+        {
+        }
+
+        internal Task GetAsync(string siteUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
