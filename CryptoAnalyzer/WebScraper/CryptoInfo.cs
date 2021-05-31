fe43c66b8c5c3b015e3c3ad5151cb8176072e544
Rytis Cepulis <cepRytis@gmail.com>
2021-05-31 22:33:57 +0300

    unneeded classes

--- CryptoAnalyzer/WebScraper/CryptoInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebScraper
-{
-    public class CryptoInfo
-    {
-        public string Token { get; set; }
-        public bool Known { get; set; }
-    }
-}
--- CryptoAnalyzer/WebScraper/IHtmlPage.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebScraper
-{
-    internal interface IHtmlPage
-    {
-    }
-}
