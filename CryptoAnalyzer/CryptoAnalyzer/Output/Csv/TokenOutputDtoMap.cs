2ead3be09e157bdc7ce1686aeae6b1bc1ff1983b
Rytis Cepulis <cepRytis@gmail.com>
2021-06-07 18:57:06 +0300

    output numeric specifier isnt needed to be so accurate

--- CryptoAnalyzer/CryptoAnalyzer/Output/Csv/TokenOutputDtoMap.cs
+++ CryptoAnalyzer/CryptoAnalyzer/Output/Csv/TokenOutputDtoMap.cs
@@ -7,9 +7,11 @@ namespace CryptoAnalyzer
     {
         public TokenOutputDtoMap()
         {
+            //var numSpec = "0." + new string('#', 339);
+            var numSpec = "F4";
             AutoMap(CultureInfo.InvariantCulture);
-            Map(m => m.TotalInaccurateValue).Convert(x => x.Value.TotalInaccurateValue.ToString("0." + new string('#', 339)));
-            Map(m => m.TotalValue).Convert(x => x.Value.TotalValue.ToString("0." + new string('#', 339)));
+            Map(m => m.TotalInaccurateValue).Convert(x => x.Value.TotalInaccurateValue.ToString(numSpec));
+            Map(m => m.TotalValue).Convert(x => x.Value.TotalValue.ToString(numSpec));
         }
     }
 }
