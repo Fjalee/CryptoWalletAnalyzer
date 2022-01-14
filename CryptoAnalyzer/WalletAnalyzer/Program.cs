a6ef234b32058c26af1411ced76e48d22a861e09
Rytis Cepulis <cepRytis@gmail.com>
2022-01-14 16:45:50 +0200

    changed from ninejct to ms dependency inject, easier to implement automapper

--- CryptoAnalyzer/WalletAnalyzer/Program.cs
+++ CryptoAnalyzer/WalletAnalyzer/Program.cs
@@ -1,27 +1,54 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using WebScraper;
 using WebScraper.Parsers;
+using WebScraper.WebScrapers;
+
 namespace WalletAnalyzer
 {
     public class Program
     {
         static async Task Main()
         {
-            DI.Initialize();
-            var config = DI.Create<IConfiguration>();
-            var dexCollector = DI.Create<IDexCollector>();
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            var dexCollector = serviceProvider.GetService<DexCollector>();
+            var config = serviceProvider.GetService<IConfiguration>();
 
             var url = @"https://etherscan.io/dextracker_txns?q=0x6b3595068778dd592e39a122f4f5a5cf09c90fe2";
             var sleepTimeMs = Int32.Parse(config["SLEEP-TIME-IN-MILISECONDS"]);
             var appendPeriodInMs = Int32.Parse(config.GetSection("OUTPUT")["APPEND-PERIOD-IN-SECONDS"]);
 
             await dexCollector.Start(url, sleepTimeMs, appendPeriodInMs);
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSingleton<IConfiguration>(SetupConfiguration())
+                .AddTransient<IDexOutput, DexCsvOutput>()
+                .AddTransient<IParserCommon, ParserCommon>()
+                .AddTransient<IWebScraper, WebScraper.WebScraper>()
+                .AddTransient<IDexTableParser, EtherscanDexParser>()
+                .AddTransient<IDexScraper, EtherscanDexScraper>()
+                .AddTransient<IDexCollector, DexCollector>();
+        }
+
+        private static IConfiguration SetupConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", false)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
         }
     }
 }
