2f81ddceac281d27c7eaba7a0cfdcf55f897bd74
Rytis Cepulis <cepRytis@gmail.com>
2022-01-15 17:05:57 +0200

    misstypes

--- CryptoAnalyzer/WebScraper/Parsers/Interfaces/IParserCommon.cs
+++ CryptoAnalyzer/WebScraper/Parsers/Interfaces/IParserCommon.cs
@@ -4,7 +4,7 @@ namespace WebScraper.Parsers
 {
     public interface IParserCommon
     {
-        void StepIfMatches(ref IElement current, string v, string className, IElement element);
+        void StepIfMatches(ref IElement current, string actualAttribute, string expectedAttribute, IElement nextStep);
         string GetDataIfMatches(string result, string actualAttribute, string expectedAttribute);
     }
 }
