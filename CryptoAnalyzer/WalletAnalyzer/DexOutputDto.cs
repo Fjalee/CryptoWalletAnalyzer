6a4906d80ab359aa1518660c6cedf7fbba5441a2
Rytis Cepulis <cepRytis@gmail.com>
2022-01-15 20:43:14 +0200

    txn might be usefull to have in the output

--- CryptoAnalyzer/WalletAnalyzer/DexOutputDto.cs
+++ CryptoAnalyzer/WalletAnalyzer/DexOutputDto.cs
@@ -6,6 +6,7 @@ namespace CryptoAnalyzer
 {
     public class DexOutputDto : IRow
     {
+        public string TxnHash { get; set; }
         public DateTime TxnDate { get; set; }
         public DexAction Action { get; set; }
         public string BuyerHash { get; set; }
