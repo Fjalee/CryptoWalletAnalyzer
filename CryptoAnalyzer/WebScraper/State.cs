75f67aa9a448d461ceac24376f861b5d7659e42e
Rytis Cepulis <cepRytis@gmail.com>
2022-07-18 00:13:28 +0300

    switching to logger instead of console write, more readable, easier to debug

--- CryptoAnalyzer/WebScraper/State.cs
+++ CryptoAnalyzer/WebScraper/State.cs
@@ -1,4 +1,5 @@
 ﻿using AngleSharp.Html.Dom;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics;
 
@@ -8,21 +9,26 @@ namespace WebScraper
     {
         public static IHtmlDocument CurrentScrapingPageHtml { get; set; }
         public static string ScrapeDate { get; set; }
-        public static void ExitAndLog(StackTrace stackTrace)
+        public static void ExitAndLog(StackTrace stackTrace, ILogger logger)
         {
             var methodName = stackTrace.GetFrame(0).GetMethod().Name;
+            var outerHtml = "";
+
+            logger.LogCritical("\nScrape date: " + ScrapeDate + "\n" +"\nSomething went wrong in method " + methodName +
+                ", OuterHtml will be printed out in the log file.");
 
             try
             {
-                Trace.WriteLine("\nScrape date: " + ScrapeDate + "\n" + CurrentScrapingPageHtml.Children[0].OuterHtml);
-                Console.WriteLine("\nERROR: Something went wrong in method " + methodName + "\nHtml page was printed into the log file...");
+                outerHtml  = CurrentScrapingPageHtml.Children[0].OuterHtml;
             }
             catch
             {
-                Console.WriteLine("\nERROR: Could not get OuterHtml of the page...");
+                logger.LogCritical("\nCould not get OuterHtml of the page.");
             }
 
-            Console.WriteLine("Press any key to exit...");
+            logger.LogCritical("\n" + outerHtml + "\n\n\n");
+
+            logger.LogInformation("Press any key to exit...");
             Console.Read();
             Environment.Exit(0);
         }
